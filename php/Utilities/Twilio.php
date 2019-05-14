cf1617e15e11dfd7892e8c3455ccd59b2aba14a8
abdu997 <aamou094@uottawa.ca>
2019-05-14 11:06:52 -0400

    removed secret keys

--- php/Utilities/Twilio.php
+++ php/Utilities/Twilio.php
@@ -6,21 +6,15 @@ use Twilio\Rest\Client;
 
 class Twilio
 {
-  static $sid = "AC9f8df6139ad54e070d5f604bd6bafddc";
-
-  static $token = "0eb1c0860de24a761fafbe347ee19f5a";
-
-  static $thawrih_number = "+16479578419";
-
   /**
-   * Sends whatsapp mesage using thawrih's twilio number to a number concatenated with the area code. Message is also concatenated with a signature.
+   * Sends whatsapp mesage using ascms's twilio number to a number concatenated with the area code. Message is also concatenated with a signature.
    *
    * @param  string $phone_number
    * @param  string $message
    */
   public static function sendWhatsapp($phone_number, $message)
   {
-    $client = new Client(self::$sid, self::$token);
+    $client = new Client($GLOBALS['sid'], $GLOBALS['token']);
     $client->messages->create(
       "whatsapp:+1".$phone_number,
       [
@@ -31,18 +25,18 @@ class Twilio
   }
 
   /**
-   * Sends sms mesage using thawrih's twilio number to a number concatenated with the area code. Message is also concatenated with a signature.
+   * Sends sms mesage using ascms's twilio number to a number concatenated with the area code. Message is also concatenated with a signature.
    *
    * @param  string $phone_number
    * @param  string $message
    */
   public static function sendSMS($phone_number, $message, $first_name, $last_name)
   {
-    $client = new Client(self::$sid, self::$token);
+    $client = new Client($GLOBALS['sid'], $GLOBALS['token']);
     $client->messages->create(
       "+1".$phone_number,
       [
-        'from' => self::$thawrih_number,
+        'from' => $GLOBALS['system_phone_number'],
         'body' => "Hello ".$first_name." ".$last_name.", ".$message." - ASCMS",
       ]
     );
